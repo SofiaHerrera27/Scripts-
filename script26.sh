27b80dfe559515c0936e0d329690ab2fe1d0a340
Meli Yañez <142264768+MeliRosita@users.noreply.github.com>
2024-04-27 14:29:35 -0500

    Scripts 26-37

---
+++ script26.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Embedding Ctl-H in a string.
+a="^H^H" # Two Ctl-H's -- backspaces
+# ctl-V ctl-H, using vi/vim
+echo "abcdef" # abcdef
+echo
+echo -n "abcdef$a " # abcd f
+# Space at end ^ ^ Backspaces twice.
+echo
+echo -n "abcdef$a" # abcdef
+# No space at end ^ Doesn't backspace (why?).
+# Results may not be quite as expected.
+echo; echo
+# Constantin Hagemeier suggests trying:
+# a=$'\010\010'
+# a=$'\b\b'
+# a=$'\x08\x08'
+# But, this does not change the results.
+########################################
+# Now, try this.
+rubout="^H^H^H^H^H" # 5 x Ctl-H.
+echo -n "12345678"
+sleep 2
+echo -n "$rubout"
+sleep 2
